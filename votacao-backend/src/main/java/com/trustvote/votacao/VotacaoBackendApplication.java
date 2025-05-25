7c3a5bc90201e06b840a85adea837baacc6ac067
GabrielFAlves <gabrielfirmamento2@gmail.com>
2025-05-25 14:05:15 -0300

    application

--- votacao-backend/src/main/java/com/trustvote/votacao/VotacaoBackendApplication.java
+++ votacao-backend/src/main/java/com/trustvote/votacao/VotacaoBackendApplication.java
@@ -2,12 +2,18 @@ package com.trustvote.votacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class VotacaoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotacaoBackendApplication.class, args);
 	}
-
 }

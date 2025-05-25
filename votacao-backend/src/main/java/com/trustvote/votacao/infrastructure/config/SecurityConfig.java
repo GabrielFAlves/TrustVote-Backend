06fad49a31436a97174679500ceb24fb290c62f4
GabrielFAlves <gabrielfirmamento2@gmail.com>
2025-05-25 14:32:24 -0300

    profile never t

--- votacao-backend/src/main/java/com/trustvote/votacao/infrastructure/config/SecurityConfig.java
+++ votacao-backend/src/main/java/com/trustvote/votacao/infrastructure/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.trustvote.votacao.infrastructure.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("never")
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;

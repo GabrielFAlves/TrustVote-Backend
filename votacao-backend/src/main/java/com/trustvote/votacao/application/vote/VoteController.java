43bce70debd0d9b5bdcd3782b98f0413f3c2b776
GabrielFAlves <gabrielfirmamento2@gmail.com>
2025-06-02 19:48:39 -0300

    Fix: cors

--- votacao-backend/src/main/java/com/trustvote/votacao/application/vote/VoteController.java
+++ votacao-backend/src/main/java/com/trustvote/votacao/application/vote/VoteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/votes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class VoteController {
 
     private final SmartContractService contractService;
